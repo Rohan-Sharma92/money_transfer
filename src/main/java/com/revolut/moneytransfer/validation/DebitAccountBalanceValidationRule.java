5c09305edc3b81718771d616d2f5e94e4efc31ee
rohan.sharma <ronsharma1992@gmail.com>
2020-02-22 13:22:56 +0530

    Refactoring

--- src/main/java/com/revolut/moneytransfer/validation/DebitAccountBalanceValidationRule.java
+++ src/main/java/com/revolut/moneytransfer/validation/DebitAccountBalanceValidationRule.java
@@ -14,6 +14,9 @@ import com.revolut.moneytransfer.util.MoneyTransferUtil;
 
 public class DebitAccountBalanceValidationRule implements IValidationRule {
 
+	private static final String CURRENCY = "currency";
+	private static final String AMOUNT = "amount";
+	private static final String DEBIT_ACCOUNT = "debitAccount";
 	private final IGenericRepository<IBalance, String> balanceCache;
 	private String errorMessage = StringUtils.EMPTY;
 
@@ -24,17 +27,17 @@ public class DebitAccountBalanceValidationRule implements IValidationRule {
 
 	@Override
 	public boolean isValid(Map<String, Object> requestMap) {
-		if (requestMap.containsKey("debitAccount") && requestMap.containsKey("amount")
-				&& requestMap.containsKey("currency")) {
-			String balanceKey = MoneyTransferUtil.generateBalanceKey(requestMap.get("debitAccount").toString(),
-					(Currency.valueOf((String) requestMap.get("currency"))));
+		if (requestMap.containsKey(DEBIT_ACCOUNT) && requestMap.containsKey(AMOUNT)
+				&& requestMap.containsKey(CURRENCY)) {
+			String balanceKey = MoneyTransferUtil.generateBalanceKey(requestMap.get(DEBIT_ACCOUNT).toString(),
+					(Currency.valueOf((String) requestMap.get(CURRENCY))));
 			IBalance existingBalance = balanceCache.findById(balanceKey);
 			if (existingBalance == null) {
 				errorMessage = "Invalid currency for debit account";
 				return false;
 			}
 			BigDecimal expectedBalance = existingBalance.getAmount()
-					.subtract(BigDecimal.valueOf((Double) requestMap.get("amount")));
+					.subtract(BigDecimal.valueOf((Double) requestMap.get(AMOUNT)));
 			if (expectedBalance.compareTo(BigDecimal.ZERO) < 0) {
 				errorMessage = "Debit account balance is insufficient";
 				return false;
